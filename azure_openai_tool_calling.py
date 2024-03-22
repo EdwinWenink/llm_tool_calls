1e533d0b90902e76bba2443f3f53ccce08293477
Edwin Wenink <edwinwenink@hotmail.com>
2024-03-22 16:34:10 +0000

    Uncomment conversation runs

--- azure_openai_tool_calling.py
+++ azure_openai_tool_calling.py
@@ -163,14 +163,14 @@ def run_conversation(user_query: str, tools=None):
 
 if __name__ == "__main__":
     # For this weather query, a tool call is available
-    # run_conversation("What's the weather like today in Nijmegen, Netherlands?", tools=tools)
+    run_conversation("What's the weather like today in Nijmegen, Netherlands?", tools=tools)
 
     # As a weather question about a place where Fahrenheit is used
     # run_conversation("What's the weather like today in George Town, Cayman Islands?", tools=tools)
     run_conversation("What's the weather like today in New York, United States?", tools=tools)
 
     # This query is completely unrelated, but we pass tools anyways
-    # run_conversation("Tell me something interesting", tools=tools)
+    run_conversation("Tell me something interesting", tools=tools)
 
     # This query is completely unrelated and we do not pass tools
-    # run_conversation("Tell me something interesting", tools=None)
+    run_conversation("Tell me something interesting", tools=None)
